8cba7a2e4a0cd992374e399035a4161451de4e60
kSzmit <zinsek@gmail.com>
2018-03-10 12:12:13 +0100

    Added proxy operator

--- basicCase/Methods.py
+++ basicCase/Methods.py
@@ -23,7 +23,14 @@ def average_of_derivatives(matrix_x, vector_y, beta):
     return avg_vec/len(matrix_x)
 
 
-def sag(beta, matrix_x, vector_y, gamma, divisor_param = None):
+def lasso_proxy_operator(beta, gamma, lam):
+    if lam is None or beta is None or gamma is None:
+        raise ValueError("Error! Not expected None values: beta = " + str(beta) + ", gamma = " + str(gamma)
+                         + ", lambda = " + str(lam))
+    return [ np.sign(x)*max((math.fabs(x) - gamma*lam), 0) for x in beta]
+
+
+def sag(beta, matrix_x, vector_y, gamma, proximal_op=None, lam=None, divisor_param=None):
     if divisor_param is None:
         divisor_param = len(matrix_x)
 
@@ -38,15 +45,18 @@ def sag(beta, matrix_x, vector_y, gamma, divisor_param = None):
                                                                                     estimated_beta))
         estimated_beta = estimated_beta - gamma * (
             (average_table.get_item(j) - derivative_phi_j_old)/divisor_param + average_table.average)
+        # only lasso prox operator
+        if proximal_op is not None:
+            estimated_beta = lasso_proxy_operator(beta, gamma, lam)
 
     return estimated_beta
 
 
-def saga(beta, matrix_x, vector_y, gamma,):
-    return sag(beta, matrix_x, vector_y, gamma, 1)
+def saga(beta, matrix_x, vector_y, gamma, proximal_op=None, lam=None):
+    return sag(beta, matrix_x, vector_y, gamma, proximal_op, lam, 1)
 
 
-def svrg(beta, matrix_x, vector_y, gamma):
+def svrg(beta, matrix_x, vector_y, gamma, proximal_op=None, lam=None):
     n = len(matrix_x)
     estimated_beta, tmp_beta = beta, beta
 
@@ -60,6 +70,10 @@ def svrg(beta, matrix_x, vector_y, gamma):
             estimated_beta_derivative = compute_derivative_f_i(matrix_x[j], vector_y[j], estimated_beta)
 
             tmp_beta = tmp_beta - gamma*(tmp_beta_derivative - estimated_beta_derivative + gradient_avg)
+
+        # no idea if proxy should be applied in inner our outer loop
+        if proximal_op is not None:
+            tmp_beta = lasso_proxy_operator(beta, gamma, lam)
 
         estimated_beta = tmp_beta
 
@@ -75,7 +89,8 @@ p = [compute_p_i(x[i],beta) for i in range(len(x))]
 y = np.random.binomial(np.ones((len(x),), dtype=int), p)
 
 print("maine saga:",saga([1, 1], x, y, 1/10))
+print("maine sag:",sag([1, 1], x, y, 1/10))
 print("maine svrg:",svrg([1, 1], x, y, 1/10))
-lr = linear_model.LogisticRegression(solver='saga')
+lr = linear_model.LogisticRegression(solver='sag')
 fit = lr.fit(x,y)
 print("not maine fit:", lr.coef_)
