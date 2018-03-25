bf5ea1ea2cacc947d88786eaa9d191859a418b88
Asia <asiaa93@o2.pl>
2018-03-25 17:23:26 +0200

    tests for ex2

--- basicCase/Results.py
+++ basicCase/Results.py
@@ -62,133 +62,183 @@ iter = 1
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+#################################
+##EXERCISE2##
+### Initial parameters ###
+
+
+
+methods = np.array(["saga", "sag", "svrg", "sgd", "bgd"])
+methods_no_bgd = np.array(["saga", "sag", "svrg", "sgd"])
+d = 20
+beta = [ 1.  ,       -1.54825395, -0.    ,     -1.45473619 ,-0. ,       -0.,
+ -0.   ,      -1.13889831 ,-1.6919646,  -0.  ,        1.21008055 , 0.,
+  0.  ,        0.    ,      0.    ,     0.    ,     0.    ,      1.04094063,
+  0.  ,        0.        ]
+gamma = None
+n_vec = np.array([200, 400, 600, 800, 1000])
+iter = 10
+
+
+
+##################
 ### Exercise 2 ###
+###################
+
 
 ### Initial parameters ###
 
-beta=seed_wrap_function(create_beta, [2, d])
+
+##################
+### 1. MSE plot ####
+#################
+
+#MSE for LASSO and compare without proxy
+
+# for cov_type in np.array([ "constant", "autocorrelation","id"]):
+#     for rho in np.array([0.3, 0.6, 0.9]):
+#          if_proxy=1
+#          if cov_type=="id":
+#              rho=0
+#              lam=0.001
+#          if (cov_type=="autocorrelation" and rho==0.3) or (cov_type=="constant" and rho==0.6):
+#              lam=0.003
+#          elif (cov_type=="autocorrelation" and rho==0.6) or (cov_type=="autocorrelation" and rho==0.9):
+#              lam=0.002
+#          elif (cov_type=="constant" and rho==0.3):
+#              lam=0.001
+#          elif (cov_type=="constant" and rho==0.9):
+#              lam = 0.0015
+#          if __name__ == "__main__":
+#              print(plot_mse(methods=methods, d=d, n_vec=n_vec, rho=rho, beta=beta, cov_type=cov_type, lasso=if_proxy, repeat=iter, lam=lam))
+#
+#          if cov_type=="id":
+#              break
+
+###############################
+####### 2. Execution time plot ###
+###############################
+
+######### for Sag  saga svrg sgd"
 
 
-# ## rho and cov parameters to test
-rho = 0.3
-cov_type = "id"
+# for cov_type in np.array([ "constant", "autocorrelation","id"]):
+#     for rho in np.array([0.3, 0.6, 0.9]):
+#         results = np.zeros((len(n_vec), len(methods_no_bgd)))
+#         k = 0
+#         if cov_type=="id":
+#             rho=0
+#         for n in n_vec:
+#             mean_vec = np.repeat(0, d-1)
+#             cov_matrix = generate_cov_matrix(d - 1, rho, cov_type)
+#             x = np.concatenate((np.ones((n, 1)), np.random.multivariate_normal(mean_vec, cov_matrix, n)), axis=1)
+#             p = [compute_p_i(x[i], beta) for i in range(len(x))]
+#             y = np.random.binomial(np.ones((len(x),), dtype=int), p)
+#             lassocv = linear_model.LassoCV(fit_intercept=1, alphas=np.arange(0.001, 0.05, 0.001))
+#             lassocv.fit(x, y)
+#             lam = lassocv.alpha_ / 10
+#             j = 0
+#             for m in methods_no_bgd:
+#                 results[k, j] = timer(m+"(x, y, proximal_op=1, lam=lam, gamma=gamma)", "from __main__ import x, y, lam, gamma;" +
+#                                                    "from basicCase.Methods import compute_p_i, saga, sag, svrg," +
+#                                                    "sgd; from basicCase.Utils import timer", repeat=iter)
+#                 j = j+1
+#             k = k+1
+#         plt.xlabel('N')
+#         plt.ylabel('Time')
+#         plt.title('LASSO - RHO = ' + str(rho)+ ", "+str(cov_type))
+#         for i in range(len(methods_no_bgd)):
+#             plt.plot(n_vec, results[:,i], '.-', label=methods_no_bgd[i])
+#         leg = plt.legend(loc='best', ncol=2)
+#         leg.get_frame().set_alpha(0.5)
+#         plt.savefig(fname=str(cov_type) + str(rho) + "_time.png")
+#         plt.close()
+#         if cov_type=="id":
+#             break
 
-### MSE plot ###
+######### 3. for bgd##
 
-if __name__ == "__main__":
-      print(plot_mse(methods=methods, d=d, n_vec=n_vec, rho=rho, beta=beta, cov_type=cov_type, lasso=1, repeat=iter))
-
-
-### MSE plot ###
-
-if __name__ == "__main__":
-     print(plot_mse(methods=methods, d=d, n_vec=n_vec, rho=rho, beta=beta, cov_type=cov_type, lasso=1, repeat=iter))
-
-### Execution time plot ###
-
-    # results = np.zeros((len(n_vec), len(methods)))
-    # k = 0
-    # for n in n_vec:
-    #     mean_vec = np.repeat(0, d-1)
-    #     cov_matrix = generate_cov_matrix(d-1, rho, cov_type)
-    #     x = np.concatenate((np.ones((n, 1)), np.random.multivariate_normal(mean_vec, cov_matrix, n)), axis=1)
-    #     beta = np.concatenate(([1], np.random.binomial(np.ones((d-1,), dtype=int), 0.25)), axis=0)
-    #     beta_vals = np.concatenate(([1], np.concatenate([np.array(np.random.uniform(-2, -1, math.floor((d-1)/2))),np.array(np.random.uniform(1, 2, math.ceil((d-1)/2)))])), axis=0)
-    #     beta = np.multiply(beta_vals, beta)
-    #     p = [compute_p_i(x[i], beta) for i in range(len(x))]
-    #     y = np.random.binomial(np.ones((len(x),), dtype=int), p)
-    #     lassocv = linear_model.LassoCV(fit_intercept=1, alphas=np.arange(0.001, 0.05, 0.001))
-    #     lassocv.fit(x, y)
-    #     lam = lassocv.alpha_ / 10
-    #     j = 0
-    #     for m in methods:
-    #         results[k, j] = timer(m+"(x, y, proximal_op=1, lam=lam, gamma=gamma)", "from __main__ import x, y, lam, gamma;" +
-    #                                            "from basicCase.Methods import compute_p_i, saga, sag, svrg," +
-    #                                            "sgd, bgd; from basicCase.Utils import timer", repeat=iter)
-    #         j = j+1
-    #     k = k+1
-    # plt.xlabel('N')
-    # plt.ylabel('Time')
-    # plt.title('LASSO - RHO = ' + str(rho))
-    # for i in range(len(methods)):
-    #     plt.plot(n_vec, results[:,i], '.-', label=methods[i])
-    # leg = plt.legend(loc='best', ncol=2)
-    # leg.get_frame().set_alpha(0.5)
-    # plt.show()
+# for cov_type in np.array([ "constant", "autocorrelation","id"]):
+#     for rho in np.array([0.3, 0.6, 0.9]):
+#         results = np.zeros((len(n_vec), 1))
+#         k = 0
+#         if cov_type=="id":
+#             rho=0
+#         for n in n_vec:
+#             mean_vec = np.repeat(0, d-1)
+#             cov_matrix = generate_cov_matrix(d - 1, rho, cov_type)
+#             x = np.concatenate((np.ones((n, 1)), np.random.multivariate_normal(mean_vec, cov_matrix, n)), axis=1)
+#             p = [compute_p_i(x[i], beta) for i in range(len(x))]
+#             y = np.random.binomial(np.ones((len(x),), dtype=int), p)
+#             lassocv = linear_model.LassoCV(fit_intercept=1, alphas=np.arange(0.001, 0.05, 0.001))
+#             lassocv.fit(x, y)
+#             lam = lassocv.alpha_ / 10
+#             j = 0
+#             for m in (["bgd"]):
+#                 results[k, j] = timer(m+"(x, y, proximal_op=1, lam=lam, gamma=gamma)", "from __main__ import x, y, lam, gamma;" +
+#                                             "from basicCase.Methods import compute_p_i, bgd;" +
+#                                             "from basicCase.Utils import timer", repeat=iter)
+#                 j = j+1
+#             k = k+1
+#         plt.xlabel('N')
+#         plt.ylabel('Time')
+#         plt.title('LASSO - RHO =' + str(rho)+ ", "+str(cov_type))
+#         plt.plot(n_vec, results[:,0], '.-', label="bgd")
+#         leg = plt.legend(loc='best', ncol=2)
+#         leg.get_frame().set_alpha(0.5)
+#         plt.savefig(fname=str(cov_type) + str(rho) + "_bgdtime.png")
+#         plt.close()
+#         if cov_type=="id":
+#             break
 
 
-
-##-----------------------------KONIEC
-
-
-
+#################
+## 4. LAMBDA SYM PLOT
+#################
 
 
-
-
-
-
-
-
-
-
-
-
-## STARE TESTY NIKOMU NIEPOTRZEBNE NA KONCU DO WYRZUCENIA ALE TERAZ MOZE SIE PRZYDAC
-### Exercise 2 - tests ###
-
-# beta = np.random.binomial(np.ones((d,), dtype=int), 0.25)
-# beta_vals = np.concatenate([np.array(np.random.uniform(-2, -1, math.floor(d/2))),np.array(np.random.uniform(1, 2, math.ceil(d/2)))])
-# beta = np.multiply(beta_vals,beta)
-# # beta = [-1.03495018,-0.,-0.,-0.,-0.,-0. ,-1.63825899,-1.75587161,-0.,-0.,0.,1.17102776 ,0.,	0.,	0.,	0.,	1.44381657,	0. ,0.,	0.	]
+# mean_vec = np.repeat(0, d-1)
+# n=500
+# x = np.concatenate((np.ones((n, 1)), np.random.multivariate_normal(mean_vec, cov_matrix, n)), axis=1)
 # p = [compute_p_i(x[i], beta) for i in range(len(x))]
 # y = np.random.binomial(np.ones((len(x),), dtype=int), p)
-# lassocv = linear_model.LassoCV( fit_intercept=False, alphas=np.arange(0.001,0.05, 0.001))
-# lassocv.fit(x, y)
-# lam = lassocv.alpha_
+# lam_vec = np.arange(0, 0.006, 0.0002)
 #
 # if __name__ == "__main__":
-#      print("--------------------------------------------------------")
-#      print("EXERCISE 2")
-#      print("--------------------------------------------------------")
-#      print("PROJECT RESULTS - MSE")
-#
-#      print("10 iter for saga  proxy MSE: ", sym(method="saga", beta=beta, x=x, y=y, gamma=gamma, proximal_op=1, lam=lam, fit_intercept=0, iter=iter))
-#      print("10 iter for sag  proxy MSE: ", sym(method="sag", beta=beta, x=x, y=y, gamma=gamma, proximal_op=1, lam=lam, fit_intercept=0, iter=iter))
-#      print("10 iter for svrg  proxy MSE: ", sym(method="svrg", beta=beta, x=x, y=y, gamma=gamma, proximal_op=1, lam=lam, fit_intercept=0, iter=iter))
-#      print("10 iter for sgd  proxy MSE: ", sym(method="sgd", beta=beta, x=x, y=y, gamma=gamma, proximal_op=1, lam=lam, fit_intercept=0, iter=iter))
-#      #print("10 iter for bgd  proxy MSE: ", sym(method="bgd", beta=beta, x=x, y=y, gamma=gamma, proximal_op=1, lam=lam, fit_intercept=0, iter=iter))
-#
-#      print("--------------------------------------------------------")
-#      print("EXERCISE 2 no PROXY")
-#      print("--------------------------------------------------------")
-#      print("PROJECT RESULTS - MSE")
-#      print("10 iter for saga no proxy MSE: ", sym(method="saga", beta=beta, x=x, y=y, gamma=gamma, proximal_op=None, lam=None, fit_intercept=0, iter=iter))
-#      print("10 iter for sag no proxy MSE: ", sym(method="sag", beta=beta, x=x, y=y, gamma=gamma, proximal_op=None, lam=None, fit_intercept=0, iter=iter))
-#      print("10 iter for svrg no proxy MSE: ", sym(method="svrg", beta=beta, x=x, y=y, gamma=gamma, proximal_op=None, lam=None, fit_intercept=0, iter=iter))
-#      print("10 iter for sgd no proxy MSE: ", sym(method="sgd", beta=beta, x=x, y=y, gamma=gamma, proximal_op=None, lam=None, fit_intercept=0, iter=iter))
-#      #print("10 iter for bgd no proxy MSE: ", sym(method="bgd", beta=beta, x=x, y=y, gamma=gamma, proximal_op=None, lam=None, fit_intercept=0, iter=iter))
-#
-#      print("--------------------------------------------------------")
-#      print("PROJECT RESULTS - TIME")
-#      print("--------------------------------------------------------")
-#      print("SAGA:", timer("saga(x, y,proximal_op=1, lam=lam, gamma=gamma)", "from __main__ import x, y, lam, gamma;" +
-#                                           "from basicCase.Methods import compute_p_i, saga, sag, svrg," +
-#                                           "stochastic_gradient_descent, batch_gradient_descent;" +
-#                                           "from basicCase.Utils import timer"), "sec.")
-#      print("SAG:", timer("sag(x, y,proximal_op=1, lam=lam, gamma=gamma)", "from __main__ import x, y, lam, gamma;" +
-#                                           "from basicCase.Methods import compute_p_i, saga, sag, svrg," +
-#                                           "stochastic_gradient_descent, batch_gradient_descent;" +
-#                                           "from basicCase.Utils import timer"), "sec.")
-#      print("SVRG:", timer("svrg(x, y,proximal_op=1, lam=lam, gamma=gamma)", "from __main__ import x, y, lam, gamma;" +
-#                                           "from basicCase.Methods import compute_p_i, saga, sag, svrg," +
-#                                           "stochastic_gradient_descent, batch_gradient_descent;" +
-#                                           "from basicCase.Utils import timer"), "sec.")
-#      print("SGD:", timer("stochastic_gradient_descent(x, y,proximal_op=1, lam=lam, gamma=gamma)", "from __main__ import x, y, lam, gamma;" +
-#                                           "from basicCase.Methods import compute_p_i, saga, sag, svrg," +
-#                                           "stochastic_gradient_descent, batch_gradient_descent;" +
-#                                           "from basicCase.Utils import timer"), "sec.")
-#      print("BGD:", timer("batch_gradient_descent(x, y,proximal_op=1, lam=lam, gamma=gamma)", "from __main__ import x, y, lam, gamma;" +
-#                                           "from basicCase.Methods import compute_p_i, saga, sag, svrg," +
-#                                           "stochastic_gradient_descent, batch_gradient_descent;" +
-#                                           "from basicCase.Utils import timer"), "sec.")
+#     for cov_type in np.array(["constant", "autocorrelation", "id"]):
+#         for rho in np.array([0.3, 0.6, 0.9]):
+#             results = np.zeros((len(lam_vec), len(methods)))
+#             k=0
+#             for lam in lam_vec:
+#                 mean_vec = np.repeat(0, d-1)
+#                 cov_matrix = generate_cov_matrix(d-1, rho, cov_type)
+#                 x = np.concatenate((np.ones((n, 1)), np.random.multivariate_normal(mean_vec, cov_matrix, n)), axis=1)
+#                 np.random.seed(100)
+#                 p = [compute_p_i(x[i], beta) for i in range(len(x))]
+#                 y = np.random.binomial(np.ones((len(x),), dtype=int), p)
+#                 j = 0
+#                 for m in methods:
+#                     results[k, j] = sym(method=m, beta=beta, x=x, y=y, gamma=None, proximal_op=1, lam=lam, fit_intercept=1, iter=iter)
+#                     j = j+1
+#                 k = k+1
+#             plt.xlabel('lam')
+#             plt.ylabel('MSE')
+#             plt.title('RHO = ' + str(rho) + ', ' + cov_type )
+#             for i in range(len(methods)):
+#                 plt.plot(lam_vec, results[:,i], '.-', label=methods[i])
+#             leg = plt.legend(loc='best', ncol=2)
+#             leg.get_frame().set_alpha(0.5)
+#             plt.savefig(fname=str(cov_type) + str(rho) +".png")
+#             plt.close()
